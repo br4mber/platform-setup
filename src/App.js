89261c052eb190d91ee669ee6bf3ed97b64c9b31
Amin Omarouayche <personal.amin@gmail.com>
2022-10-12 17:54:38 +0100

    form finally working

--- src/App.js
+++ src/App.js
@@ -15,12 +15,13 @@ const App = () => {
               name: "welcome-screen",
               id: "jg1401r",
               attributes: {
-                label: "Welcome to our survey",
-                description: "This is just a description",
+                buttonText: "Let's go",
+                label: "Start your Campaign today",
+                description: "Setting up a Trussle campaign is as easy as answering a few questions!",
                 attachment: {
-                  type: "image",
-                  url:
-                    "https://quillforms.com/wp-content/uploads/2022/01/4207-ai-1.jpeg"
+                  //type: "image",
+                  //url:
+                  //  "https://quillforms.com/wp-content/uploads/2022/01/4207-ai-1.jpeg"
                 }
               }
             },
@@ -29,7 +30,7 @@ const App = () => {
               id: "kd12edg",
               attributes: {
                 required: true,
-                label: "Let's start with your name"
+                label: "Let's start with giving your campaign a name"
               }
             },
             {
@@ -56,103 +57,28 @@ const App = () => {
                 label: "Please type your birth of date!"
               }
             },
-            {
-              name: "email",
-              id: "iqfrqwr13r",
-              attributes: {
-                required: true,
-                label: "Thanks {{field:kdsfkdg}}, please insert your email!"
-              }
-            },
-            {
-              name: "dropdown",
-              id: "nb913rqw",
-              attributes: {
-                required: true,
-                label: "Please select one choice",
-                choices: [
-                  {
-                    label: "Choice 1",
-                    value: "choice-1"
-                  },
-                  {
-                    label: "Choice 2",
-                    value: "choice-2"
-                  },
-                  {
-                    label: "Choice 2",
-                    value: "choice-3"
-                  }
-                ]
-              }
-            },
-            {
-              name: "multiple-choice",
-              id: "gqr1294c",
-              attributes: {
-                required: true,
-                multiple: true,
-                verticalAlign: false,
-                label: "Which subjects do you love the most?",
-                choices: [
-                  {
-                    label: "Physics",
-                    value: "physics"
-                  },
-                  {
-                    label: "Math",
-                    value: "math"
-                  },
-                  {
-                    label: "English",
-                    value: "english"
-                  },
-                  {
-                    label: "Biology",
-                    value: "biology"
-                  }
-                ]
-              }
-            },
-            {
-              name: "statement",
-              id: "g91imf1023",
-              attributes: {
-                label: "You are doing great so far!",
-                buttonText: "Continue",
-                quotationMarks: true
-              }
-            },
-            {
-              name: "website",
-              id: "bv91em9123",
-              attributes: {
-                required: true,
-                multiple: true,
-                label: "Please insert your website url!"
-              }
-            }
           ],
           settings: {
-            animationDirection: "vertical",
+            animationDirection: "horizontal",
             disableWheelSwiping: false,
-            disableNavigationArrows: false,
+            disableNavigationArrows: true,
             disableProgressBar: false
           },
           theme: {
-            font: "Roboto",
-            buttonsBgColor: "#9b51e0",
+            //font: "Roboto",
+            buttonsBgColor: "#2F5061",
+            backgroundColor: "#f2efe4",
             logo: {
               src: ""
             },
             questionsColor: "#000",
-            answersColor: "#0aa7c2",
+            answersColor: "#2F5061",
             buttonsFontColor: "#fff",
-            buttonsBorderRadius: 25,
+            buttonsBorderRadius: 10,
             errorsFontColor: "#fff",
             errorsBgColor: "#f00",
-            progressBarFillColor: "#000",
-            progressBarBgColor: "#ccc"
+            progressBarFillColor: "#2F5061",
+            progressBarBgColor: "#f2efe4"
           }
         }}
         onSubmit={(data, { completeForm, setIsSubmitting }) => {
